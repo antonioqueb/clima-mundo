eaf80897241f247591929058c949b87ec435de62
Antonio Queb <cardstillo@gmail.com>
2023-02-21 20:06:57 -0600

    initial commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+//Importamos el estado que queremos usar
+
+const Home = () => {
+
+    return (
+        <div>
+            <h1>Hola, soy el home</h1>
+        </div>
+    );
+   
+};
+
+export default Home;
